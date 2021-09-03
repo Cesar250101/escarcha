3c97bb7013029b46fa174c47059c54abfa9ea771
Your Name <you@example.com>
2021-09-03 15:10:51 -0400

    first commit

--- models/models.py
+++ models/models.py
@@ -15,6 +15,7 @@ class ModuleName(models.Model):
     precio_sin_impto = fields.Char(compute='_compute_precio_sin_impto', string='Precio S/Impto')
     
     @api.depends('quantity','price_unit')
-    def _compute_precio_sin_impto(self):
+    @api.one
+    def _compute_precio_sin_impto(self):        
         self.precio_sin_impto= self.price_unit-(self.price_tax/self.quantity)
 
