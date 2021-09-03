ab4936e68388f264ccbcf18edf3c30d1cfdc517f
Your Name <you@example.com>
2021-09-03 15:06:39 -0400

    first commit

--- models/models.py
+++ models/models.py
@@ -7,3 +7,14 @@ class ModuleName(models.Model):
     _inherit = 'res.company'
 
     logo_propyme = fields.Binary(string='Logo ProPyme')
+
+
+class ModuleName(models.Model):
+    _inherit = 'account.invoice.line'
+
+    precio_sin_impto = fields.Char(compute='_compute_precio_sin_impto', string='Precio S/Impto')
+    
+    @api.depends('quantity','price_unit')
+    def _compute_precio_sin_impto(self):
+        self.precio_sin_impto= self.price_unit-(self.price_tax/self.quantity)
+
